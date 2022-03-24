cac4809ce85ee4d1e4fcc6f5c0a13fbf6a79154a
mihailValkov <mihail_valko@mail.bg>
2022-03-24 20:14:16 +0200

    implement auth reducers

---
+++ client/src/app/+store/reducers.ts
@@ -0,0 +1,67 @@
+import { createReducer, on } from '@ngrx/store';
+import { IUser } from '../shared/interfaces/user';
+import * as authActions from './actions';
+
+export interface IAuthState {
+  user: IUser | undefined | null;
+  isLoading: boolean;
+  message: string | null;
+}
+
+const initialAuthState: IAuthState = {
+  user: undefined,
+  isLoading: false,
+  message: null,
+};
+
+const setUser = (
+  state: IAuthState,
+  {
+    user,
+  }:
+    | ReturnType<typeof authActions.loginSuccess>
+    | ReturnType<typeof authActions.registerSuccess>
+    | ReturnType<typeof authActions.authenticateSuccess>
+) => ({
+  ...state,
+  user,
+  message: null,
+  isLoading: false,
+});
+
+const setErrorMessage = (
+  state: IAuthState,
+  { message }: { message: string }
+) => ({
+  ...state,
+  message,
+  isLoading: false,
+});
+
+const startFetching = (state: IAuthState) => ({
+  ...state,
+  user: null,
+  isLoading: true,
+  message: null,
+});
+
+export const authReducer = createReducer<IAuthState>(
+  initialAuthState,
+  on(authActions.loginStart, startFetching),
+  on(authActions.loginSuccess, setUser),
+  on(authActions.loginFailure, setErrorMessage),
+  on(authActions.loginClearError, (state) => ({ ...state, message: null })),
+  on(authActions.registerStart, startFetching),
+  on(authActions.registerSuccess, setUser),
+  on(authActions.registerFailure, setErrorMessage),
+  on(authActions.registerClearError, (state) => ({ ...state, message: null })),
+  on(authActions.authenticateStart, startFetching),
+  on(authActions.authenticateSuccess, setUser),
+  on(authActions.authenticateFailure, (state, action) => ({
+    ...state,
+    isLoading: false,
+    user: null,
+  })),
+  on(authActions.logoutStart, startFetching),
+  on(authActions.logoutEnd, (state) => ({ ...state, user: null, isLoading: false }))
+);
