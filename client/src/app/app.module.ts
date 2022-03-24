e1c0c30bb35b6df01eeb8ae53453363a2a5463df
mihailValkov <mihail_valko@mail.bg>
2022-03-24 20:19:06 +0200

    connect app with ngrx store

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,35 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material/material.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material/material.module';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import { reducers } from './+store';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     CoreModule,
-    HttpClientModule,
-    MaterialModule,
+    BrowserModule,
     AppRoutingModule,
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    StoreModule.forRoot(reducers),
     EffectsModule.forRoot([]),
-    StoreModule.forRoot({}, {}),
+    environment.production ? [] : StoreDevtoolsModule.instrument(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
