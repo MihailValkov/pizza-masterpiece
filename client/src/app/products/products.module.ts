24f514fdadaafb760be1f2c54053135fd4c23e97
mihailValkov <mihail_valko@mail.bg>
2022-04-03 16:17:14 +0300

    added components to product module

--- client/src/app/products/products.module.ts
+++ client/src/app/products/products.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductItemComponent,
+    ProductsListComponent,
+    ProductDetailComponent
+  ],
   imports: [
     CommonModule
   ]
